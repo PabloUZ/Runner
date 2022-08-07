cdbe307cfae74be5645a7a457b7dc63f09500336
Pablo Uribe <puribezuluaga28@gmail.com>
2022-08-06 20:12:21 -0500

    Cambio de id

--- extension.js
+++ extension.js
@@ -11,16 +11,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "runner" is now active!');
+	console.log('Congratulations, your extension "vsc-runner" is now active!');
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('runner.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('vsc-runner.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from runner!');
+		vscode.window.showInformationMessage('Hello World from vsc-runner!');
 	});
-	let run = vscode.commands.registerTextEditorCommand('runner.runcode', function () {
+	let run = vscode.commands.registerTextEditorCommand('vsc-runner.runcode', function () {
 		let file = String(vscode.window.activeTextEditor.document.fileName);
 		let ext;
 		let nfile = file.split('\\');
@@ -28,7 +28,7 @@ function activate(context) {
 		nfile = nfile[nfile.length - 1].split('.');
 		file = nfile[0];
 		ext = nfile[1];
-		
+
 		let _terminal;
 		if(vscode.window.activeTerminal!=null) {
 			vscode.commands.executeCommand('workbench.action.terminal.killAll')
@@ -38,7 +38,7 @@ function activate(context) {
 		vscode.window.showInformationMessage("Running "+fileId);
 		_terminal.sendText(execMap(fileId, file, ext));
 	});
-	
+
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(run);
 }
